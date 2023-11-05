271909a4497068fbdc6f2b0200ad18346524f5d5
sarah-zhan <yizh.contact@gmail.com>
2023-11-04 17:19:35 -0700

    load the model and add parameters

--- app.py
+++ app.py
@@ -5,29 +5,7 @@ import pickle
 import xgboost as xgb
 import numpy as np
 
-# load model
-modle_file = "model.bin"
-
-with open(modle_file, "rb") as f_in:
-    dv, model = pickle.load(f_in)
-
-X = pd.DataFrame(dv.get_feature_names_out())
-
-
-"""
-main features:
-yearbuilt: Original construction date
-grlivarea: Above grade (ground) living area square feet
-garagecars: Size of garage in car capacity
-centralair=y: Central air conditioning
-totalbsmtsf: Total square feet of basement area
-bsmtfinsf1: Type 1 finished square feet
-1stflrsf: First Floor square feet
-lotarea: Lot size in square feet
-masvnrarea: Masonry veneer area in square feet
-garageyrblt: Year garage was built
-"""
-
+#
 st.write("""# House Price Prediction
 This app predicts the house price in **Ames, Iowa**""")
 st.write('---')
@@ -36,16 +14,16 @@ st.sidebar.header("Specify Your Parameters")
 
 # customed features input function
 def user_input_features():
-    yearbuilt = st.sidebar.slider("Year Built", X.yearbuilt.min(), X.yearbuilt.max(), X.yearbuilt.mean())
-    grlivarea = st.sidebar.slider("Above ground living space", X.grlivarea.min(), X.grlivarea.max(), X.grlivarea.mean())
-    garagecars = st.sidebar.slider("Garage size (hold how many car?)", X.garagecars.min(), X.garagecars.max(), X.garagecars.mean())
-    centralair = st.sidebar.slider("Central air conditioning", X["centralair=y"].min(), X["centralair=y"].max())
-    totalbsmtsf = st.sidebar.slider("Total square feet of basement area", X.totalbsmtsf.min(), X.totalbsmtsf.max(), X.totalbsmtsf.mean())
-    bsmtfinsf1 = st.sidebar.slider("Good Living Quarters square feet", X.bsmtfinsf1.min(), X.bsmtfinsf1.max(), X.bsmtfinsf1.mean())
-    firstflrsf = st.sidebar.slider("First Floor square feet", X["1stflrsf"].min(), X["1stflrsf"].max(), X["1stflrsf"].mean())
-    lotarea = st.sidebar.slider("Lot size in square feet", X["lotarea"].min(), X["lotarea"].max(), X["lotarea"].mean())
-    masvnrarea = st.sidebar.slider("Masonry veneer area in square feet", X["masvnrarea"].min(), X["masvnrarea"].max(), X["masvnrarea"].mean())
-    garageyrblt = st.sidebar.slider("Year garage was built", X["garageyrblt"].min(), X["garageyrblt"].max(), X["garageyrblt"].mean())
+    yearbuilt = st.sidebar.slider("Year Built", min_value=1800, max_value=2024, value=2000, step=1)
+    grlivarea = st.sidebar.slider("Above ground living space square feet", min_value=300, max_value=7000, value=2000, step=100)
+    garagecars = st.sidebar.slider("Garage size (Fit how many cars?)", min_value=0, max_value=4, value=2, step=1)
+    centralair = st.sidebar.slider('Central air conditioning? Yes->"1"; No->"0', min_value=0, max_value=1, value=1)
+    totalbsmtsf = st.sidebar.slider("Total square feet of basement area", min_value=0, max_value=7000, value=500, step=100)
+    bsmtfinsf1 = st.sidebar.slider("Good Living Quarters square feet", min_value=0, max_value=7000, value=500, step=100)
+    firstflrsf = st.sidebar.slider("First Floor square feet", min_value=300, max_value=1200, value=500, step=100)
+    lotarea = st.sidebar.slider("Lot size in square feet", min_value=1300, max_value=8000, value=3000, step=100)
+    masvnrarea = st.sidebar.slider("Masonry veneer area in square feet", min_value=0, max_value=500, value=100, step=10)
+    garageyrblt = st.sidebar.slider("Year garage was built", min_value=1800, max_value=2024, value=2000, step=1)
 
     data = {
         "yearbuilt": yearbuilt,
@@ -63,4 +41,30 @@ def user_input_features():
     features = pd.DataFrame(data, index=[0])
     return features
 
+df = user_input_features()
+# main panel
+st.header("Specify Your Parameters")
+st.write(df)
+st.write("---")
 
+# load model
+modle_file = "model.bin"
+
+with open(modle_file, "rb") as f_in:
+    dv, model = pickle.load(f_in)
+
+def predict():
+    X = dv.transform(df.to_dict(orient="records"))
+    dx = xgb.DMatrix(X, feature_names=dv.get_feature_names_out().tolist())
+    prediction = model.predict(dx)
+    price = np.expm1(prediction)[0]
+    result = {
+        "price": float(price)
+    }
+
+    return result["price"]
+
+st.subheader("Our estimated house price")
+st.write("👇👇👇👇👇👇👇👇👇👇")
+st.write(predict())
+st.write("👆👆👆👆👆👆👆👆👆👆")
